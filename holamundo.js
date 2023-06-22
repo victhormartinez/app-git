6133b0f2750698a902c63b06f44d9039b9a28298
Dvillamizar01 <114126554+Dvillamizar01@users.noreply.github.com>
2023-06-21 21:49:21 -0500

    Update holamundo.js

--- holamundo.js
+++ holamundo.js
@@ -2,4 +2,12 @@ const express = require('express');
 const holamundo = express();
 holamundo.get('/', (req, res) =>
 res.send('Hola Mundo!'));
+
+holamundo.get('/resta/:num1/:num2', (req, res) => {
+    const num1 = parseInt(req.params.num1);
+    const num2 = parseInt(req.params.num2);
+    const resultado = num1 - num2;
+    res.send(`El resultado de la resta es: ${resultado}`);
+});
+
 module.exports = holamundo;
